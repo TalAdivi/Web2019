5f49362fa0f4f2a8f4fa0a8cabcc7549aa1b0354
TalAdivi <taladivi10@gmail.com>
2019-05-24 20:35:14 +0300

    update first flow

--- Semester B web 2019/exercises/firstFlow/includes/main.js
+++ Semester B web 2019/exercises/firstFlow/includes/main.js
@@ -5,14 +5,12 @@ $( function() {
         progressbar = $( ".progress-bar" ),
         progressbarValue = progressbar.find( ".progress-bar" );
         
-        console.log(progressbar);
 
        if ( target.is( "#colorButton" ) ) {
            console.log('here');
         
            progressbar.css({"background-color": '#' + Math.floor( Math.random() * 16777215 ).toString( 16 )});
 
-        console.log( progressbar.css);
       } else if ( target.is( "#falseButton" ) ) {
           if(!clicked){
               progressbar.addClass("progress-bar-striped");
@@ -26,5 +24,34 @@ $( function() {
             clicked = false;
           }
       }
+
     });
-  } );
+
+    $('.addIcon').on("click",function(){
+
+        var table_row = $(
+            '<tr>' +
+            '<td>' +
+            '<input type="text" class="form-control" placeholder="Product name">' +
+            '</td>' +
+            '<td>' +
+            '<input type="number" class="form-control" placeholder="0">' +
+            '</td>' +
+            '</tr>'
+        )
+        $('.mainTableBody').append(table_row);
+    })
+
+    // to remove selected meal
+    $("button").on("click",function(curr){
+            $("button").removeClass("active");
+            $(this).toggleClass("active");
+    })
+
+  });
+  
+
+
+
+
+
