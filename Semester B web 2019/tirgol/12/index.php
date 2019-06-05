73966605d24d53c894ce88097b7f90c4aba3fa3f
TalAdivi <taladivi10@gmail.com>
2019-06-05 19:46:09 +0300

    upload

---
+++ Semester B web 2019/tirgol/12/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>forms</title>
+        <link rel="stylesheet"
+href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3
+/css/bootstrap.min.css"
+integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdE
+fwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+    </head>
+<body>
+    <h1> Form Parameter List <br> </h1>
+    <section>
+        <?php
+        $colors = ["red","blue","yellow"];
+        
+        echo "<p> i love the colors " .[0] . " and " . [1] . ", but my sister likes the color " .[3] . "</p";
+        ?>
+    </section>
+ 
+</body>
+    </html>
